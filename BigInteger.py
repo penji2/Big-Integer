e4d8babde50d03212233915de94573467ca9bb69
penji2 <krystian.pegowski@gmail.com>
2019-04-16 18:00:12 +0200

    projects almost done, karatsuba need to be fixed, z1 z2 and z3 have to be object instances of BigInteger

---
+++ BigInteger.py
@@ -0,0 +1,141 @@
+import numpy as np
+import random
+import math
+
+
+class BigInteger:
+    def __init__(self, data):
+        self.a_number = data
+
+    @staticmethod
+    def convert_to_str(array):
+        # converting integer list to string list
+        s = [str(i) for i in array]
+        result = int("".join(s))
+        return str(result)
+
+    def printing(self):
+        print(self.a_number)
+
+    def add(self, x):
+        result = []
+        guard = 0
+        if len(x.a_number) < len(self.a_number):
+            x.a_number = BigInteger.fill_with_zeroes(x.a_number, len(self.a_number))
+        for i in range(len(self.a_number) - 1, - 1, -1):
+            tmp = int(self.a_number[i]) + int(x.a_number[i])
+
+            if tmp + guard < 10:
+                result.append(tmp + guard)
+                guard = 0
+            else:
+                result.append(tmp + guard - 10)
+                guard = 1
+        if guard == 1:
+            result.append(guard)
+        result.reverse()
+        result_object = BigInteger(BigInteger.convert_to_str(result))
+        # return BigInteger.convert_to_str(result)
+        return result_object, BigInteger.convert_to_str(result)
+
+    @staticmethod
+    def fill_with_zeroes(string, length):
+        return string.zfill(length)
+
+    def subtract(self, x):
+        result = []
+        guard = 0
+        if len(x.a_number) < len(self.a_number):
+            x.a_number = BigInteger.fill_with_zeroes(x.a_number, len(self.a_number))
+        for i in range(len(self.a_number) - 1, - 1, -1):
+            tmp = int(self.a_number[i]) - int(x.a_number[i])
+
+            if tmp - guard >= 0:
+                result.append(tmp - guard)
+                guard = 0
+            else:
+                result.append(tmp + 10 - guard)
+                guard = 1
+        result.reverse()
+        result_object = BigInteger(BigInteger.convert_to_str(result))
+        return result_object, BigInteger.convert_to_str(result)
+
+    @staticmethod
+    def split_string(string, len_of_num):
+        str1 = BigInteger('')
+        str2 = BigInteger('')
+        for i in range(len_of_num):
+            if i < len_of_num / 2:
+                str1.a_number += string[i]
+            else:
+                str2.a_number += string[i]
+        return str1, str2
+
+    def multiplying(self, x):
+        """number has a form : BigNumber1 = high1*B**len(object.a_number) + low1"""
+        # if len(x.a_number) < len(self.a_number):
+        #     x.a_number = BigInteger.fill_with_zeroes(x.a_number, len(self.a_number))
+        if len(self.a_number) < 3 or len(x.a_number) < 3:
+            string_result = str(int(self.a_number) * int(x.a_number))
+            result = BigInteger(string_result)
+            return result
+
+        high1, low1 = BigInteger.split_string(self.a_number, len(self.a_number))
+        high2, low2 = BigInteger.split_string(x.a_number, len(x.a_number))
+
+        z0 = low1.multiplying(low2)
+
+        z1 = (low1.add(high1)[0]).multiplying(low2.add(high2)[0])
+
+        z2 = high1.multiplying(high2)
+
+        return (z2.multiplying(10**(len(self.a_number)))).add(((z1.subtruct(z2)[0].subtruct(z0))[0].multiplying(10**(len(self.a_number)//2).add(z0)[0])))
+
+    def factorial(self):
+        unit = BigInteger('1')
+        if self.a_number == unit:
+            return unit
+
+        return self.multiplying((self.subtract(unit)[0]).factorial())
+
+
+def testing(x):
+    if x == 0:
+        return 1
+    return x * testing(x - 1)
+
+    pass
+
+
+def make_random_string(length_of_table, max_number):
+    random_table = np.random.randint(1, max_number, length_of_table)
+    string = BigInteger.convert_to_str(random_table)
+    return string
+
+
+def main():
+    big_number1 = make_random_string(100, 9)
+    big_number2 = make_random_string(100, 9)
+    x1 = BigInteger(big_number1)
+    x2 = BigInteger(big_number2)
+
+    "printing"
+    print('x1 =         ', end='')
+    x1.printing()
+    print('x2 =         ', end='')
+    x2.printing()
+
+    "adding"
+    print('adding:     ', x1.add(x2))
+
+    "subtracting"
+    print('subtracting:', x1.subtract(x2))
+
+    "multiplying"
+    print('multiplying:', x1.multiplying(x2))
+
+    "factorial"
+    # print('factorial: ', x1.factorial())
+
+
+main()
